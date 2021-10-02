ac2387b5f80fbe4cba89f6212e41eb65f9fe79a6
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2021-10-01 22:45:07 -0300

    ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { stripe } from '../services/stripe'
 interface HomeProps {
   product: {
@@ -9,6 +9,10 @@ interface HomeProps {
     amount: number,
   }
 }
+
+// client side = useEffect
+//serverside = getServerSideProps
+// estatico = getStaticProps
 
 export default function Home({product} : HomeProps) {
 
@@ -34,7 +38,7 @@ export default function Home({product} : HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => { //carrega a pagina uma vez de acordo com o revalidade.
   const price =  await stripe.prices.retrieve("price_1JfwXgDqVzUtyqwcW7Fi20ip")
 
   const  product  = {
@@ -49,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 horas - tempo de revalidacao (reconstrução da pagina)
   };
 }
