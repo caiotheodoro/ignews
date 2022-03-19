36bcc9abbb05d50886a34c308ff147d6e225a739
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-18 22:58:33 -0300

    Test enviroment + teste button

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports ={
-    testeIgnorePatterns: ['/node_modules/', '/.next/'],
+    testePathIgnorePatterns: ['/node_modules/', '/.next/'],
     setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
     transform: {
         '^.+\\.(jsx|ts|tsx|js)$': '<rootDir>/node_modules/babel-jest',
 
     },
-    testeEnviroment: 'jsdom',
+    testEnvironment: 'jsdom',
 }
