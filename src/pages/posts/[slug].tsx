bd010e153d48ae615e6ce7788e473edf663aa07a
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-20 20:50:12 -0300

    style moved

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -3,7 +3,7 @@ import { getSession } from "next-auth/client"
 import Head from "next/head"
 import { RichText } from "prismic-dom"
 import { getPrismicClient } from "../../services/prismic"
-import styles from './post.module.scss'
+import styles from '../../styles/post.module.scss';
 
 interface PostProps {
     post: {
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/dist/shared/lib/head';
 import { getPrismicClient } from '../../services/prismic';
-import styles from './styles.module.scss';
+import styles from '../../styles/styles.module.scss';
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom';
 import Link from 'next/link';
