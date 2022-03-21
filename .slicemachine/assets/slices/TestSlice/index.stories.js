15581c9ab49313edcc81d4a63676d84dcee3014b
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-20 21:13:39 -0300

    slice prismic

---
+++ .slicemachine/assets/slices/TestSlice/index.stories.js
@@ -0,0 +1,11 @@
+import MyComponent from './../../../../slices/TestSlice';
+
+export default {
+  title: 'slices/TestSlice'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"id":"_DefaultSlice","variation":"default-slice","name":"Default slice","docURL":"...","version":"sktwi1xtmkfgx8626","description":"Test Slice","primary":{"title":[{"type":"heading1","text":"This is where it all begins...","spans":[]}],"description":[{"type":"paragraph","text":"start by editing this slice from inside the SliceMachine builder!","spans":[]}]}}} />
+_DefaultSlice.storyName = 'Default slice'
+
+ 
---
+++ slices/index.js
@@ -0,0 +1,1 @@
+export { default as TestSlice } from './TestSlice'
